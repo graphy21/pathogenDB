3e0aaff5aa14d422861f30d4a1661c068369dcf9
Sehwan Kim <graphy21@gmail.com>
2015-06-12 15:31:47 +0900

    make entrance page, but should complete authentication

---
+++ pathogenDB/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import os
+
+
+def index(request):
+	PATH_SOURCE = os.path.dirname(os.path.abspath(__file__))
+	#return render(request, '{}/index.html'.format(PATH_SOURCE))
+	return render(request, 'index.html')
