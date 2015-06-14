7ac33338c32e7b641a8eb77b1556dd5079d990c8
Sehwan Kim <graphy21@gmail.com>
2015-06-14 14:08:07 +0900

    complete entrance page

--- pathogenDB/urls.py
+++ pathogenDB/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index),
 	url(r'^pathogenSite/', include('pathogenSite.urls')),
 	url(r'^taxon-group/', include('ssuProkSite.urls')),
 ]
