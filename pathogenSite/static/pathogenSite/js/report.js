9845750b4952113f5cf148d64cd92c4a3177a69f
Sehwan Kim <graphy21@gmail.com>
2015-07-03 10:10:48 +0900

    execute sorting sample correctly, but don't know how

--- pathogenSite/static/pathogenSite/js/report.js
+++ pathogenSite/static/pathogenSite/js/report.js
@@ -33,8 +33,24 @@ $(document).ready(function(){
 		$('#sortable').children().each(function () {
 			order.push($(this).text());
 		});
+
+		// update barChart
 		speciesPerSampleBarChart.x(d3.scale.ordinal().domain(order));
 		lineChart.x(d3.scale.ordinal().domain(order));
+
+		// update "indexToSample", "sampleToIndex"
+		for (var i = 0, max = order.length; i < max; i += 1) {
+			indexToSample[i] = order[i];
+			sampleToIndex[order[i]]	= i;
+		}
+		console.log('222222', indexToSample, sampleToIndex);
+
+		// update lineChart
+		/*
+		speciesPerSamples2 = makeDataForLineChart();
+		console.log('fffff', speciesPerSamples2[0]['value'].all(), speciesPerSamples2[1]['value'].all());
+		drawLineChart(speciesPerSamples2);
+		*/
 		dc.redrawAll();
 	});
 });
@@ -60,6 +76,7 @@ var topGenusCount = 5,
 		return d.count;
 	}),
 	indexToSample = {},
+	sampleToIndex = {},
 
 	genusDim = microbiome.dimension(function (d){
 		return d.genus;    
@@ -74,15 +91,16 @@ var topGenusCount = 5,
 		return d.count;
 	}),
 
-	top_species = speciesDimGroup.top(topSpeciesCount),
-	top_species_name = [],
+	topSpecies = speciesDimGroup.top(topSpeciesCount),
+	topSpeciesName = [],
 
-	species_per_samples = [];
+	speciesPerSamples = [],
+	speciesPerSamples2 = [];
 
 /* populate variables 
  * */
-for (var i = 0, max = top_species.length; i < max; i +=1 ){
-	top_species_name.push(top_species[i].key);
+for (var i = 0, max = topSpecies.length; i < max; i +=1 ){
+	topSpeciesName.push(topSpecies[i].key);
 }
 
 function getSampleDimGroupByType(rank, type) {
@@ -92,17 +110,17 @@ function getSampleDimGroupByType(rank, type) {
 			} else {
 				return 0;
 			}
-		});}
+});}
 
-for (var i = 0, max = top_species_name.length; i < max; i += 1){
-	var species = top_species_name[i];
+for (var i = 0, max = topSpeciesName.length; i < max; i += 1){
+	var species = topSpeciesName[i];
 	var value = getSampleDimGroupByType('species', species);
-	species_per_samples.push( {'key':species, 'value': value} );
+	speciesPerSamples.push( {'key':species, 'value': value} );
 }
 
-species_per_samples.push({'key': 'etc', 'value': 
+speciesPerSamples.push({'key': 'etc', 'value': 
 	sampleDim.group().reduceSum( function (d) {
-			if (top_species_name.indexOf(d['species']) === -1){
+			if (topSpeciesName.indexOf(d['species']) === -1){
 				return d.count;
 			} else {
 				return 0;
@@ -112,7 +130,84 @@ species_per_samples.push({'key': 'etc', 'value':
 for (var i=0, max=sampleDimGroup.all().length; i < max; i += 1){
 	var t = sampleDimGroup.all()[i];
 	indexToSample[i] = t.key;
+	sampleToIndex[t.key] = i;
 }
+console.log('1', indexToSample, sampleToIndex);
+
+
+
+/* test for linechart to bypass error
+ * */
+//function checkSampleToIndex (
+
+function getSampleDimGroupByType2(rank, type){
+	console.log('sssss', sampleToIndex);
+	var sti = sampleToIndex;
+	console.log('stististi', sti);
+	var group = sampleDim.group().reduce(
+		function (p, d) {
+			if (d[rank] === type){
+				p.count += d.count;
+				console.log('wwww', sampleToIndex);
+			}
+			p.index = sampleToIndex[d.sample];
+			return p;
+		},
+		function (p, d) {
+			if (d[rank] === type){
+				p.count -= d.count;
+			}
+			p.index = sampleToIndex[d.sample];
+			return p;
+		},
+		function () { return {'count':0, 'index':0} }
+	);
+	group.all = function(){
+		var result = [];
+		var tt = {};
+		var ref = group.top(Infinity);
+		for (var i = 0, max = ref.length; i < max; i+= 1){
+			var sampleName = ref[i].key;
+			var index = sampleToIndex[sampleName];
+			tt[index] = ref[i];
+		}
+		for (var i = 0, max = ref.length; i < max; i += 1){
+			result.push(tt[i]);
+		}
+		console.log('1212121', sampleToIndex);
+		console.log('ttttttttttt', result);
+		return result;
+
+	}
+	return group;
+}
+
+function makeDataForLineChart () {
+	var groups = [];
+	for (var i = 0, max = topSpeciesName.length; i < max; i += 1){
+		var species = topSpeciesName[i];
+		var value = getSampleDimGroupByType2('species', species);
+		groups.push( {'key':species, 'value': value} );
+	}
+
+	groups.push({'key': 'etc', 'value': 
+		sampleDim.group().reduceSum( function (d) {
+				if (topSpeciesName.indexOf(d['species']) === -1){
+					return d.count;
+				} else {
+					return 0;
+				}
+			})});
+
+	for (var i=0, max=sampleDimGroup.all().length; i < max; i += 1){
+		var t = sampleDimGroup.all()[i];
+		indexToSample[i] = t.key;
+	sampleToIndex[t.key] = i;
+	}
+	return groups;
+}
+
+
 
 /* draw chart 
  * */
@@ -124,7 +219,6 @@ samplePieChart
 	.dimension(sampleDim)
 	.group(sampleDimGroup)
 	.label(function (d) {
-		console.log('dddd',d);
 		return d.data.key;
 	})
 	.valueAccessor(function(d){
@@ -150,13 +244,13 @@ speciesPerSampleBarChart
 	.height(300)
 	.margins({top:20, right:20, bottom:30, left:50})
 	.dimension(sampleDim)
-	.group(species_per_samples[0]['value'], species_per_samples[0]['key'])
+	.group(speciesPerSamples[0]['value'], speciesPerSamples[0]['key'])
 	.valueAccessor(function(p) {
 		return p.value;
 	});
 
-for (var i = 1, max = species_per_samples.length; i < max; i+=1){
-	var comp = species_per_samples[i];
+for (var i = 1, max = speciesPerSamples.length; i < max; i+=1){
+	var comp = speciesPerSamples[i];
 	speciesPerSampleBarChart.stack(comp['value'], comp['key']);
 }
 speciesPerSampleBarChart
@@ -171,27 +265,35 @@ speciesPerSampleBarChart
 		return d.y;
 	});
 
-lineChart
-	.renderArea(true)
-	.width(790)
-	.height(300)
-	.margins({top:20, right:20, bottom:30, left:50})
-	.dimension(sampleDim)
-	.valueAccessor(function(p){
-		return p.value;
-	})
-	.group(species_per_samples[0]['value'], species_per_samples[0]['key']);
 
-for (var i = 1, max = species_per_samples.length; i < max; i+=1){
-	var comp = species_per_samples[i];
-	lineChart.stack(comp['value'], comp['key']);
+function drawLineChart (groups){
+	console.log('ccccc', groups[0]['value'].all(), groups[1]['value'].all());
+	lineChart
+		.renderArea(true)
+		.xUnits(dc.units.ordinal)
+		.width(790)
+		.height(300)
+		.margins({top:20, right:20, bottom:30, left:50})
+		.dimension(sampleDim)
+		.valueAccessor(function(p){
+			return p.value.count;
+		})
+		.group(groups[0]['value'], groups[0]['key']);
+
+	for (var i = 1, max = groups.length; i < max; i+=1){
+		var comp = groups[i];
+		lineChart.stack(comp['value'], comp['key']);
+	}
+	lineChart
+		.renderHorizontalGridLines(true)
+		.x(d3.scale.ordinal())
+		.xUnits(dc.units.ordinal)
+		.elasticX(true)
+		.elasticY(true);
 }
-lineChart
-	.renderHorizontalGridLines(true)
-	.x(d3.scale.ordinal())
-	.xUnits(dc.units.ordinal)
-	.elasticX(true)
-	.elasticY(true);
+speciesPerSamples2 = makeDataForLineChart();
+console.log('fffff', speciesPerSamples2[0]['value'].all(), speciesPerSamples2[1]['value'].all());
+drawLineChart(speciesPerSamples2);
 
 
 dc.renderAll();
