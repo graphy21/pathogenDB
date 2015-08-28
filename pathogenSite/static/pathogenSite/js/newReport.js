5a0a9a6b65b2fb2aace9168173babf258a330f89
Sehwan Kim <graphy21@gmail.com>
2015-08-28 18:39:24 +0900

    complete table & start to add line in plot

--- pathogenSite/static/pathogenSite/js/newReport.js
+++ pathogenSite/static/pathogenSite/js/newReport.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function () {
 
 	function parseDataForDC(preData) {
@@ -47,7 +48,8 @@ $(document).ready(function () {
 		var rank = model.ranks[i];
 		model[rank+"Dim"] = model.cr.dimension(function (d) {return d[rank];});
 		model[rank+"DimGroup"] = model[rank+"Dim"].group().reduceSum(
-			function (d) {return d.count / totalCount[d.sample];}
+			//function (d) {return d.count / totalCount[d.sample];}
+			function (d) {return d.count;}
 		);
 		model[rank+"DimGroupInfo"] = model[rank+"Dim"].group().reduce(
 			function (p, d) {
@@ -71,9 +73,25 @@ $(document).ready(function () {
 		);
     }
 
+	$.fn.dataTable.ext.type.order['count-with-percentage-pre'] = function (d) {
+		return parseInt(d.split(" ")[0].replace(",",""));
+	};
+
 	var plot = { 
 		mainPlot: dc.barChart("#main-plot"),
-		mainTable: $("#main-table").DataTable()
+		mainTable: $("#main-table").DataTable({
+			"columnDefs": [
+				{
+					//"render": function (data, type, row){
+					//	return data.toString().replace(/\B(?=(\d{3})+(?!\d))/g,
+					//	   	",") + " ( %)"
+					//},
+					"type": "count-with-percentage",
+					"targets": 1
+				}
+			],
+			"order": [[1, "desc"]]
+		})
 	};
 
 
@@ -90,28 +108,47 @@ $(document).ready(function () {
 		getTableData: function(rank) {
 			var selecteData = model[rank + "DimGroupInfo"].all();
 			var tableData = [];
+			var totalCount = 0;
+			for (var i=0,max=selecteData.length; i<max; i+=1){
+				var datum = selecteData[i],
+					values = datum.value,
+					count = values.count;
+				totalCount += count;
+			}
 			for (var i=0,max=selecteData.length; i<max; i+=1){
 				var datum = selecteData[i];
 				var values = datum.value;
+				var count = values.count;
+				var countRender = values.count.toString()
+					.replace(/\B(?=(\d{3})+(?!\d))/g, ",") + 
+						" ( " + (count/totalCount*100).toFixed(2) + "%)";
 				if (values.count != 0){
-					var pathogenInfo = {
-						pathogen_human:"",
-						pathogen_animal:"",
-						pathogen_plant:""
-					};
-					var organisms = ["human", "animal", "plant"];
-					for (var j=0,maxJ=organisms.length; j<maxJ; j+= 1){
-						var organism = organisms[j];
-						var index = "pathogen_"+organism;
-						if (values[index] === "3") {
-							pathogenInfo[index] = "primary";
-						} else if (values[index] === "4") {
-							pathogenInfo[index] = "opportunistic";
-						} else {
-							pathogenInfo[index] = "none";
+					if (rank === "species") {
+						var pathogenInfo = {
+							pathogen_human:"",
+							pathogen_animal:"",
+							pathogen_plant:""
+						};
+						var organisms = ["human", "animal", "plant"];
+						for (var j=0,maxJ=organisms.length; j<maxJ; j+= 1){
+							var organism = organisms[j];
+							var index = "pathogen_"+organism;
+							if (values[index] === "3") {
+								pathogenInfo[index] = "primary";
+							} else if (values[index] === "4") {
+								pathogenInfo[index] = "opportunistic";
+							} else {
+								pathogenInfo[index] = "none";
+							}
+						}
+					} else {
+						var pathogenInfo = {
+							pathogen_human:"-",
+							pathogen_animal:"-",
+							pathogen_plant:"-"
 						}
 					}
-					tableData.push( [datum.key, values.count, 
+					tableData.push( [datum.key, countRender, 
 							pathogenInfo["pathogen_human"], 
 							pathogenInfo["pathogen_animal"],
 							pathogenInfo["pathogen_plant"] ] );
@@ -119,6 +156,8 @@ $(document).ready(function () {
 			}
 			return tableData;
 		},
+
+		getLineData: function() {console.log('well'); return 1;},
 	
 		filterByCheckedPathogenArray: function (checkedArray) {
 			model.pathogenDim.filter(function (d) {
@@ -127,12 +166,12 @@ $(document).ready(function () {
 		},
 
 		getDataPerSample: function (rank, checkedArray, topNumber, yUnit) {
-			var parsedData = [];
 			// get top ranks 
 			var topRanks = model[rank+"DimGroup"].top(topNumber);
 			var topRankNames = [];
+			var parsedData = [];
 			for (var i=0, max=topRanks.length; i < max; i+=1){
-				topRank = topRanks[i];
+				var topRank = topRanks[i];
 				topRankNames.push(topRank['key']);
 				parsedData.push( {key: topRank['key'], value: 
 					this.getSampleDimGroupByType(rank, [topRank['key']], false,
@@ -143,7 +182,6 @@ $(document).ready(function () {
 				this.getSampleDimGroupByType(rank, topRankNames, true,
 					checkedArray, yUnit)
 			});
-
 			return parsedData;
 
 		},
@@ -152,21 +190,15 @@ $(document).ready(function () {
 				yUnit) {
 			var group = model.sampleDim.group().reduceSum( function (d) {
 				if ((exclude === true) && (types.indexOf(d[rank]) === -1)){
-					//if ((typeof checkPathogen !== 'undefined') && 
-					//	(checkPathogen.indexOf(d.is_pathogen) > -1)){
-						if (yUnit === 'percentage') {
-							return d.count / totalCount[d.sample];
-						}
-						return d.count;
-					//}
+					if (yUnit === 'percentage') {
+						return d.count / totalCount[d.sample];
+					}
+					return d.count;
 				}else if ((exclude === false) && (types.indexOf(d[rank]) > -1)){
-					//if ((typeof checkPathogen !== 'undefined') && 
-					//	(checkPathogen.indexOf(d.is_pathogen) > -1)){
-						if (yUnit === 'percentage') {
-							return d.count / totalCount[d.sample];
-						}
-						return d.count;
-					//}
+					if (yUnit === 'percentage') {
+						return d.count / totalCount[d.sample];
+					}
+					return d.count;
 				}
 				return 0;
 			});
@@ -201,7 +233,7 @@ $(document).ready(function () {
 				.transitionDuration(1000)
 				.renderHorizontalGridLines(true)
 				.legend(dc.legend().x(950).y(20).itemHeight(13).gap(5)
-						.horizontal(false).legendWidth(140).itemWidth(70))
+					.horizontal(false).legendWidth(140).itemWidth(70))
 				.group(model['sampleDimGroup']);
 			mainPlot.yAxis().tickFormat(function(v) {
 					return (v * 100).toFixed(2);});
@@ -209,7 +241,7 @@ $(document).ready(function () {
 			dc.renderAll();	
 			var options = this.checkOptions();
 			controller.filterByCheckedPathogenArray(options[1]);
-			this.renderPlot(options[0], options[1], 5, options[2]);
+			this.renderPlot(options[0], options[1], 5, options[2], 0.7);
 			this.renderTable( controller.getTableData(options[0]) );
 
 			// set each option event
@@ -233,7 +265,7 @@ $(document).ready(function () {
 			$("#rank-selector, #unit-selector, #pathogen, #nonpathogen").on(
 				"change", function () {
 					var options = view.checkOptions();
-					view.renderPlot(options[0], options[1], 5, options[2]);
+					view.renderPlot(options[0], options[1], 5, options[2], 0.4);
 					var tableData = controller.getTableData(options[0]);
 					view.renderTable(tableData);
 				}
@@ -257,12 +289,35 @@ $(document).ready(function () {
 			return [rank, checkedArray, yUnit];
 		},
 
-		renderPlot: function (rank, checkedArray, topNumber, yUnit){
+		renderPlot: function (rank, checkedArray, topNumber, yUnit, tempY){
 			var parsedData = controller.getDataPerSample(rank, checkedArray, 
 					topNumber, yUnit);
 			var mainPlot = plot.mainPlot;
 			mainPlot
 				.dimension(model.sampleDim)
+				.on("renderlet", function (chart) {
+					var options = view.checkOptions();
+					var organisms = ["human", "animal", "plant"];
+					for (var i=0,max=organisms.length; i<max; i+=1){
+						console.log('good', options, organisms[i]);
+						var organismData = controller.getLineData();
+					}
+					var left_y = 0.1, right_y = tempY; 
+					console.log('5555', chart);
+					var extra_data = [
+						{x: chart.x().range()[0]+chart.x().rangeBand()/2, y: chart.y()(left_y)}, 
+						{x: chart.x().range()[3]+chart.x().rangeBand()/2, y: chart.y()(right_y)}
+					];
+					var line = d3.svg.line() 
+						.x(function(d) { return d.x; }) 
+						.y(function(d) { return d.y; })
+						.interpolate('cardinal');
+					var path = chart.select('g.chart-body')
+						.selectAll('path.extra').data([extra_data]);
+					path.enter().append('path').attr('class', 'extra')
+						.attr('stroke', 'yellow');
+					path.attr('d', line);
+				})
 				.group(parsedData[0]['value'],parsedData[0]['key']);
 			for (var i=1, max=parsedData.length; i < max; i += 1){
 				var comp = parsedData[i];
@@ -278,6 +333,14 @@ $(document).ready(function () {
 				.clear()
 				.rows.add(tableData)
 				.draw();
+			$("tbody tr td:first-child").each(function () {
+				$(this).html("<a target='_blank'"+
+					"href='http://www.ezbiocloud.net/eztaxon/"+
+					"hierarchy?m=nomen_view&nid="+$(this).text()+"'>"+
+					$(this).text()+"</a>");
+
+			});
+
 		}
 	};
 	
